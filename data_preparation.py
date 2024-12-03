46005a89dc9c5135dc950a053011ae9209e5f93e
jayati-17 <jayatisood9@gmail.com>
2024-12-03 12:59:55 +0100

    Re-upload

--- data_preparation.py
+++ data_preparation.py
@@ -8,9 +8,17 @@ from biogeme.segmentation import DiscreteSegmentationTuple, segmented_beta
 
 # Load the data
 df = pd.read_csv("lpmc01.dat", sep = '\t')
-database = db.Database('lpmc01', df)
+df['age_scaled'] = (df['age'] - df['age'].mean()) / df['age'].std()
+df['cost_driving'] = df['cost_driving_ccharge'] + df['cost_driving_fuel']
+df['dur_pt'] = df['dur_pt_access'] + df['dur_pt_rail'] + df['dur_pt_int'] + df['dur_pt_bus']
+
+database1 = db.Database('lpmc01', df)
+
 
 # Define the given veriables 
+dur_pt = Variable('dur_pt')
+cost_driving = Variable('cost_driving')
+age_scaled = Variable('age_scaled')
 trip_id = Variable('trip_id')
 household_id = Variable('household_id')
 person_n = Variable('person_n')
@@ -46,11 +54,14 @@ driving_traffic_percent = Variable('driving_traffic_percent')
 
 
 # Define new variables:
-# Define driving cost 
-cost_driving = cost_driving_ccharge + cost_driving_fuel
+# # Define driving cost 
+# df['cost_driving'] = df['cost_driving_ccharge'] + df['cost_driving_fuel']
+# cost_driving = Variable(cost_driving_ccharge + cost_driving_fuel)
 
-# Define time taken by each mode of transport
-dur_pt = dur_pt_access + dur_pt_rail + dur_pt_bus + dur_pt_int
+# # Define time taken by each mode of transport
+# df['dur_pt'] = df['dur_pt_access'] + df['dur_pt_rail'] + df['dur_pt_int'] + df['dur_pt_bus']
+# dur_pt = Variable(dur_pt_access + dur_pt_rail + dur_pt_int + dur_pt_bus)
+
 
 # Define transport availability
 # Assume pt, walking, cycle always available, with car availability depending on number of cars per household. From the data, 
@@ -60,8 +71,23 @@ av_pt =1
 av_walk = 1
 av_cycle = 1
 
+variable_names = ['dur_pt', 'cost_driving', 'age_scaled']  # Replace with your variable name
+for variable_name in variable_names:
+    if variable_name in database1.data.columns:
+        print(f"'{variable_name}' exists in the database.")
+    else:
+        print(f"'{variable_name}' does NOT exist in the database.")
+
+
+
 # Define pt_cost (not needed)
 # Original paper, page 31: "Public transport fares are determined for single trips using Oystercard/contactless payment."
 # Therefore, cost_transit should already consider faretype and bus_scale
 
-
+database = db.Database('lpmc01', df)
+variable_names = ['dur_pt', 'cost_driving', 'age_scaled']  # Replace with your variable name
+for variable_name in variable_names:
+    if variable_name in database1.data.columns:
+        print(f"'{variable_name}' exists in the database.")
+    else:
+        print(f"'{variable_name}' does NOT exist in the database.")
